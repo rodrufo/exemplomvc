7a51d49e8bb3fc1b1f20a3a19b196f30bfb2e7a0
prof <prof@PC-PROF-C.eteot.edu>
2019-06-10 20:23:11 -0300

    Alteração do title da página.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <link href="css/estilo.css" type="text/css" media="all" rel="stylesheet">
         <link href="css/print.css" type="text/css" media="print" rel="stylesheet">
-        <title>Página inicial</title>
+        <title>Projeto Exemplo MVC - Página inicial</title>
         <script>
             var dadosCliente = <?php echo $data ?>;
         </script>
